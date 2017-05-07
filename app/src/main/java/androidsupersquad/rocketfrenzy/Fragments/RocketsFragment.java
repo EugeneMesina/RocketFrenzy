ab8551329a2fb630cbe75151fa2328441e649d22
Jimmy <iamjimmyjimmy@yahoo.com>
2017-05-06 22:15:44 -0700

    Grid Inventory

--- app/src/main/java/androidsupersquad/rocketfrenzy/Fragments/RocketsFragment.java
+++ app/src/main/java/androidsupersquad/rocketfrenzy/Fragments/RocketsFragment.java
@@ -8,16 +8,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import androidsupersquad.rocketfrenzy.Fragments.Adapters.RocketGridAdapter;
+import androidsupersquad.rocketfrenzy.Fragments.Models.Rocket;
 import androidsupersquad.rocketfrenzy.R;
 
 
 public class RocketsFragment extends Fragment {
-
+List<Rocket> rockets;
     public RocketsFragment() {
+        rockets= new ArrayList<Rocket>();
+        rockets.add(new Rocket("Apollo",R.drawable.apollorocket,"heck"));
+        rockets.add(new Rocket("Apollo",R.drawable.apollorocket,"heck"));
+        rockets.add(new Rocket("Apollo",R.drawable.apollorocket,"heck"));
+        rockets.add(new Rocket("Apollo",R.drawable.apollorocket,"heck"));
+        rockets.add(new Rocket("Apollo",R.drawable.kamakazirocket,"heck"));
+        rockets.add(new Rocket("Apollo",R.drawable.kamakazirocket,"heck"));
+
     }
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
         GridView inventory = (GridView) getActivity().findViewById(R.id.Inventory);
+        inventory.setAdapter(new RocketGridAdapter(getActivity().getBaseContext(),rockets));
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
