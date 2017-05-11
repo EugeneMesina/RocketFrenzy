4391fa0d118658be0086aed25f2fae91c244f0f8
Jimmy <iamjimmyjimmy@yahoo.com>
2017-05-11 00:20:41 -0700

    Reward System

--- app/src/main/java/androidsupersquad/rocketfrenzy/RocketData.java
+++ app/src/main/java/androidsupersquad/rocketfrenzy/RocketData.java
@@ -1,5 +1,7 @@
 package androidsupersquad.rocketfrenzy;
 
+import java.util.Random;
+
 import androidsupersquad.rocketfrenzy.Fragments.Models.Rocket;
 
 /**
@@ -10,4 +12,16 @@ public class RocketData {
     public final static Rocket ApolloRocket = new Rocket("Apollo",R.drawable.apollorocket, "Heck");
     public final static Rocket HorizonRocket=new Rocket("Horizon",R.drawable.horizonrocket, "Heck");
     public final static Rocket KamakaziRocket=new Rocket("Something",R.drawable.kamakazirocket, "Heck");
+    public static Rocket giveRocket()
+    {
+        Random giveRocket = new Random();
+        int number= giveRocket.nextInt(3);
+        switch(number)
+        {
+            case 1: return ApolloRocket;
+            case 2: return HorizonRocket;
+            case 3: return KamakaziRocket;
+            default: return ApolloRocket;
+        }
+    }
 }
